e3be33d8a3860ffc9c234155cf6f05d006c4e119
Kevin <kevin.molina@csu.fullerton.edu>
2020-06-12 17:46:09 -0400

    create a couple quick tests

--- confessions/tests.py
+++ confessions/tests.py
@@ -3,8 +3,27 @@ from django.contrib.auth.models import User
 from confessions.models import Confession, Comment, Vote
 
 
-class Foo(TestCase):
-    foo = "lol"
+class TestConfession(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testUser")
+        self.confession = Confession.objects.create(
+            user=self.user, title="title", body="body"
+        )
+        self.comment = Comment.objects.create(
+            user=self.user, confession=self.confession, comment="comment"
+        )
+        self.vote = Vote.objects.create(
+            user=self.user, confession=self.confession, vote=Vote.UPVOTE
+        )
 
-    def test_foo(self):
-        pass
+    def test_delete_confession(self):
+        self.confession.delete()
+        assert Confession.objects.count() == 0
+        assert Comment.objects.count() == 0
+        assert Vote.objects.count() == 0
+
+    def test_delete_comment(self):
+        self.comment.delete()
+        assert Confession.objects.count() == 1
+        assert Comment.objects.count() == 0
+        assert Vote.objects.count() == 1
