71dbb7c2fd8cbd6f1da4030ed48cd8da9ffcfd96
Kevin <kevin.molina@csu.fullerton.edu>
2020-06-13 02:29:17 -0400

    update subnet config

--- infra/vpc/vpc-us-east-1.tf
+++ infra/vpc/vpc-us-east-1.tf
@@ -11,23 +11,63 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc-us-east-1" {
-    cidr_block       = "10.0.0.0/20"
+    cidr_block       = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "subnet-us-east-1a" {
+// Public Subnets
+resource "aws_subnet" "public-subnet-us-east-1a" {
     vpc_id            = aws_vpc.vpc-us-east-1.id
     cidr_block        = "10.0.0.0/22"
     availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "subnet-us-east-1b" {
+resource "aws_subnet" "public-subnet-us-east-1b" {
     vpc_id            = aws_vpc.vpc-us-east-1.id
     cidr_block        = "10.0.4.0/22"
     availability_zone = "us-east-1b"
+
+    tags = {
+        Type = "Public Subnet"
+    }
 }
 
-resource "aws_subnet" "subnet-us-east-1c" {
+resource "aws_subnet" "public-subnet-us-east-1c" {
     vpc_id            = aws_vpc.vpc-us-east-1.id
     cidr_block        = "10.0.8.0/22"
     availability_zone = "us-east-1c"
+
+    tags = {
+        Type = "Public Subnet"
+    }
+}
+
+// Private Subnets
+resource "aws_subnet" "private-subnet-us-east-1a" {
+    vpc_id            = aws_vpc.vpc-us-east-1.id
+    cidr_block        = "10.0.12.0/22"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Type = "Private Subnet"
+    }
+}
+
+resource "aws_subnet" "private-subnet-us-east-1b" {
+    vpc_id            = aws_vpc.vpc-us-east-1.id
+    cidr_block        = "10.0.16.0/22"
+    availability_zone = "us-east-1b"
+
+    tags = {
+        Type = "Private Subnet"
+    }
+}
+
+resource "aws_subnet" "private-subnet-us-east-1c" {
+    vpc_id            = aws_vpc.vpc-us-east-1.id
+    cidr_block        = "10.0.20.0/22"
+    availability_zone = "us-east-1c"
+
+    tags = {
+        Type = "Private Subnet"
+    }
 }
