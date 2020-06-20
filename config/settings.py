d8ec74f725aba143532d1fd8d55c0ea6396708e8
Kevin <kevin.molina@csu.fullerton.edu>
2020-06-20 05:29:59 -0400

    update allowed hosts during ci

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG_MODE") == "True" else False
 
-ALLOWED_HOSTS = (os.getenv("ALLOWED_HOSTS"))
+ALLOWED_HOSTS = (os.getenv("ALLOWED_HOSTS"), "*")
 
 
 # Application definition
