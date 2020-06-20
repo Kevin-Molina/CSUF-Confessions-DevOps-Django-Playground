489021fbd1293545cbdb016416ad4e7d790c6b96
Kevin <kevin.molina@csu.fullerton.edu>
2020-06-20 04:52:48 -0400

    update task def templating

--- infra/task-definition/task_definition.tf
+++ infra/task-definition/task_definition.tf
@@ -3,7 +3,6 @@ variable "IMAGE_VERSION" {}
 
 resource "aws_ecs_task_definition" "csuf-confessions" {
   family = "csuf-confessions"
-  container_definitions = file("${path.module}/task.json")
-
+  container_definitions = templatefile("${path.module}/task.tmpl", {IMAGE_VERSION=var.IMAGE_VERSION, REPOSITORY_URI=var.REPOSITORY_URI})
   network_mode = "bridge"
 }
