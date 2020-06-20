3c080771fb18a4a49241cba4a8770d14a51f461e
Kevin <kevin.molina@csu.fullerton.edu>
2020-06-20 19:12:57 -0400

    shorten target deregistration delay

--- infra/alb/alb.tf
+++ infra/alb/alb.tf
@@ -66,4 +66,5 @@ resource "aws_lb_target_group" "target-group" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+  deregistration_delay = 60 # Shouldn't have long standing requests; speed up deployments. Throws 5xx errors if too short
 }
