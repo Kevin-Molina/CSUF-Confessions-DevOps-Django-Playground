65aa7e659cd84bede7ed99306cf35aa25484c998
Kevin <kevin.molina@csu.fullerton.edu>
2020-06-20 04:05:34 -0400

    update image version on ci, enabling deployment

--- infra/task-definition/task_definition.tf
+++ infra/task-definition/task_definition.tf
@@ -1,3 +1,6 @@
+variable "REPOSITORY_URI" {}
+variable "IMAGE_VERSION" {}
+
 resource "aws_ecs_task_definition" "csuf-confessions" {
   family = "csuf-confessions"
   container_definitions = file("${path.module}/task.json")
